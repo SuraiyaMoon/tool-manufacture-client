e2f7ba1fff7dc054107f48eeef06819174c8fc2c
asaduzzaman69 <=>
2022-07-22 19:00:40 +0600

    stripe work end

--- src/pages/DashBoard/CheckoutForm.js
+++ src/pages/DashBoard/CheckoutForm.js
@@ -1,11 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 
-const CheckoutForm = () => {
+const CheckoutForm = ({ order }) => {
+    const [user] = useAuthState(auth);
     const stripe = useStripe();
     const elements = useElements();
-    const [cardError, setCardError] = useState('')
+    const [cardError, setCardError] = useState('');
+    const [startLoading, setStartLoading] = useState('');
+    const [success, setSuccess] = useState('');
+    const [transactionId, setTransactionId] = useState('');
+    const [clientSecret, setClientSecret] = useState('');
+    const { price, email, _id } = order;
+    useEffect(() => {
+        fetch('http://localhost:5000/create-payment-intent', {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json',
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            },
+            body: JSON.stringify({ price })
+
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data?.clientSecret) {
+                    setClientSecret(data.clientSecret);
+                }
+                console.log(data)
+            })
+    }, [])
 
 
     const handleSubmit = async (event) => {
@@ -28,7 +54,53 @@ const CheckoutForm = () => {
 
 
         setCardError(error?.message || '')
+        setSuccess('');
+        setStartLoading(true);
 
+        //confirm payment
+
+        const { paymentIntent, error: intentError } = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: user.displayName,
+                        email: email,
+                    },
+                },
+            },
+        );
+        if (intentError) {
+            setCardError(intentError?.message)
+            setStartLoading(false)
+
+        }
+        else {
+            setCardError('')
+            setTransactionId(paymentIntent.id)
+            console.log(paymentIntent)
+            const payment = {
+                product: _id,
+                transactionId: paymentIntent.id
+            }
+
+            fetch(`http://localhost:5000/order/${_id}`, {
+                method: "PATCH",
+                headers: {
+                    'content-type': 'application/json',
+                    authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                },
+                body: JSON.stringify(payment)
+
+            })
+                .then(res => res.json())
+                .then(data => {
+                    setStartLoading(false)
+                    console.log(data)
+                })
+            setSuccess('Your payment is completed')
+        }
 
 
     }
@@ -51,12 +123,18 @@ const CheckoutForm = () => {
                         },
                     }}
                 />
-                <button className='btn btn-success btn-xs' type="submit" disabled={!stripe}>
+                <button className='btn btn-success btn-xs' type="submit" disabled={!stripe || !clientSecret}>
                     Pay
                 </button>
             </form>
             {
                 cardError && <p className='text-error'>{cardError}</p>
+            }
+            {
+                success && <div className='text-success'>
+                    <p>{success}</p>
+                    <p>Your Transaction id : <span className='font-bold'>{transactionId}</span> </p>
+                </div>
             }
 
         </>
--- src/pages/DashBoard/OrderRow.js
+++ src/pages/DashBoard/OrderRow.js
@@ -2,16 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const OrderRow = ({ order, index }) => {
-    const { tool, price, paid, _id } = order;
+    const { tool, price, paid, _id, transactionId } = order;
 
     return (
         <tr>
             <th>{index + 1}</th>
             <td>{tool}</td>
             <td>$ {price}</td>
-            <td>{(price && !paid) && <Link to={`/dashboard/payment/${_id}`}> <button className='btn btn-xs btn-success text-white'>Pay</button></Link>}</td>
-            <td>{(price && paid) && <span className='text-success'>Paid</span>}</td>
-            <td><button className='btn btn-xs '>Cancel order</button></td>
+            <td>{(price && !paid) && <div className='flex items-center'>
+                <Link to={`/dashboard/payment/${_id}`}> <button className='btn btn-xs btn-success text-white'>Pay</button></Link>
+                <td><button className='btn btn-xs '>Cancel order</button></td>
+
+            </div>}</td>
+            <td>{(price && paid) && <>
+                <span className='text-success'>Paid</span>
+                <p>Transaction id: {transactionId}</p>
+            </>}</td>
 
         </tr>
     );
