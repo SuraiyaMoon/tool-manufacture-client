7ab81318269b1dca179903e67db3cbedcff838d0
asaduzzaman69 <=>
2022-07-22 22:41:54 +0600

    status added in manage order page

--- src/pages/DashBoard/ManageOrderRow.js
+++ src/pages/DashBoard/ManageOrderRow.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const ManageOrderRow = ({ index, order }) => {
-    const { paid } = order;
+    const { paid, status } = order;
     return (
         <tr>
             <th>{index + 1}</th>
             <td>{order.tool}</td>
             {
-                paid ? <td><button className='btn btn-xs btn-success text-white'>Shipped</button></td>
+                paid ? <td><button className='btn btn-xs btn-success text-white'>{status}</button></td>
                     :
                     <>
                         <td><button className='btn btn-xs btn-success text-white'>Unpaid</button></td>
